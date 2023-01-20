d92f1c2104f9701fbdeab2fc14f940d33454bf79
Артем Шестаков <ashestakov@iMac-Artem.local>
2023-01-20 13:31:33 +0300

    Update tests

--- molecule/master-master/tests/test_role.py
+++ molecule/master-master/tests/test_role.py
@@ -1,4 +1,5 @@
 import os
+import time
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -10,9 +11,20 @@ def test_service_is_run(host):
     assert svc.is_enabled
 
 def test_ip(host):
-    if host.check_output('hostname -s') == 'instance01' and host.service('nginx').is_running:
-        ips = host.interface('eth0').addresses
-        assert len(ips) > 2
-    elif not host.service('nginx').is_running:
-        ips = host.interface('eth0').addresses
-        assert len(ips) == 1
+    assert host.service('nginx').is_running
+    ips = host.interface('eth0').addresses
+    assert len(ips) >= 2
+
+def test_ip_migration(host):
+    if host.check_output('hostname -s') == 'instance01':
+        host.run("systemctl stop nginx")
+        for i in range(0,3):
+            if len(host.interface('eth0').addresses) != 1 and i != 2:
+                time.sleep(3)
+                continue
+            else:
+                assert len(host.interface('eth0').addresses) == 1
+    if host.check_output('hostname -s') == 'instance02':
+       assert len(host.interface('eth0').addresses) == 3
+
+        
